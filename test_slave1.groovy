64ebee64f8d3faac54b0812361e22c3f59cdd3ed
devopsincubatorpoc <147982502+devopsincubatorpoc@users.noreply.github.com>
2024-02-13 15:59:19 +0530

    Update test_slave1.groovy

--- test_slave1.groovy
+++ test_slave1.groovy
@@ -11,48 +11,4 @@ pipelineJob("test-slave1") {
           branch('*/main')
         }
       }
-}			
-"""pipeline {
-    agent {
-        node ('slave1')
-    }
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
-            }
-        }
-        stage('Google') {
-            steps {
-                echo 'Building Hello World'
-                sh 'curl google.com'
-            }
-        }
-        stage('Facebook') {
-            steps {
-                echo 'Releasing Hello World'
-                sh 'curl facebook.com'
-            }
-        }
-        stage('Yahoo') {
-            steps {
-                echo 'Deploying Hello World'
-                sh 'curl yahoo.com'
-            }
-        }
-        stage('Apple') {
-            steps {
-                echo 'Testing Hello World'
-                sh 'curl apple.com'
-            }
-        }  
-        stage('Monitor') {
-            steps {
-                echo 'Monitoring Hello World'
-                sh 'curl valtech.com'
-            }
-        }       
-              
-    }
-}"""		}
-	}
+}
